4143faf7a8122066613d17d5a681b5abc8f4d0fd
kango furusu <furusu0715@gmail.com>
2019-10-25 02:10:43 +0900

    renderchart試す

---
+++ components/Chart.vue
@@ -0,0 +1,29 @@
+<!--<script>-->
+<!--import { reactive, createComponent, onMounted } from '@vue/composition-api'-->
+<!--import { Bar } from 'vue-chartjs'-->
+<!--export default createComponent({-->
+<!--  extends: Bar,-->
+<!--  props: ['data', 'options'],-->
+<!--  name: 'chart',-->
+<!--  setup(props) {-->
+<!--    onMounted(() => {-->
+<!--      this.renderChart(props.data, props.options)-->
+<!--    })-->
+<!--  }-->
+<!--})-->
+<!--</script>-->
+
+<!--<style scoped></style>-->
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartdata', 'options'],
+  mounted() {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,53 @@
 <script>
-import { createComponent, reactive } from '@vue/composition-api'
+import {
+  createComponent,
+  reactive,
+  onMounted,
+  SetupContext
+} from '@vue/composition-api'
 // import Chart from '@/components/Chart.vue'
-import { Line } from 'vue-chartjs'
+// import VueCharts from 'vue-chartjs'
+import { Bar, Line } from 'vue-chartjs'
 
 export default createComponent({
   extends: Line,
+  // setup(props, context) {
+  //   const emitSample = () => {
+  //     context.emit('emit-sample', 'サンプル')
+  //   }
+  //   const state = reactive({
+  //     test: context
+  //   })
+  //   onMounted(() => {
+  //     console.log(context)
+  //     // context.renderChart({
+  //     //   labels: [
+  //     //     'January',
+  //     //     'February',
+  //     //     'March',
+  //     //     'April',
+  //     //     'May',
+  //     //     'June',
+  //     //     'July'
+  //     //   ],
+  //     //   datasets: [
+  //     //     {
+  //     //       label: 'Data One',
+  //     //       backgroundColor: '#f87979',
+  //     //       data: [40, 39, 10, 40, 39, 80, 40]
+  //     //     }
+  //     //   ]
+  //     // })
+  //   })
+  //   return {
+  //     state
+  //   }
+  // }
+  setup(props, context) {
+    console.log(context)
+  },
   mounted() {
+    console.log(this)
     this.renderChart({
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
